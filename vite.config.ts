5731f9868b674ef41ef0c1c40f6b8746dfcd1f93
hussainkazemian <hussain.kazemian@metropolia.fi>
2025-01-29 10:34:15 +0200

    feat: update Vite configuration base path for custom hooks

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/~mattpe/hybrid-react-build/',
-  base: '',
+  base: '/~hussaink/custom-hooks/',
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest-setup.js',
   },
 });
+
